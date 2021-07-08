72f651f60c97d4603471d9e73040fdc46a181ab4
cjy0019 <cjy0029@naver.com>
2021-07-08 19:52:09 +0900

    config : 설정 파일 추가하기 [#3]
    resolved : #3
    - CONTRIBUTING.md 추가.
    - styled-reset 패키지 설치.
    - asset 디렉터리에 GlobalStyles.jsx 추가.

---
+++ fitpet/src/assets/GlobalStyles.jsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+// Global reset.css
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+`;
+
+export default GlobalStyles;
