2c3e98dc55535f4fb1924fba5b57c1858439a043
cjy0019 <cjy0029@naver.com>
2021-07-16 03:49:47 +0900

    feat : 메인페이지 헤더 네비게이션 영역 수정[#26]
    - css hover 사용해서 hover 시 언더라인.

--- fitpet/src/components/mainpage/header/MenuNavLi.jsx
+++ fitpet/src/components/mainpage/header/MenuNavLi.jsx
@@ -5,18 +5,22 @@ import styled from 'styled-components';
 const MenuNavLi = ({ children, to }) => {
   return (
     <StyledLi>
-      <NavLink
-        to={to}
-        style={{ color: '#707070' }}
-        activeStyle={{ color: '#707070' }}>
-        {children}
-      </NavLink>
+      <NavLink to={to}>{children}</NavLink>
     </StyledLi>
   );
 };
 
 const StyledLi = styled.li`
   margin-right: 4rem;
+  padding-bottom: 0.4rem;
+
+  a {
+    text-decoration: none;
+    color: #707070;
+  }
+  a:hover {
+    border-bottom: solid 0.4rem #979797;
+  }
 `;
 
 export default MenuNavLi;
