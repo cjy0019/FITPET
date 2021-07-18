596258d59addc6cf3748c72c64d8eb1646766402
cjy0019 <cjy0029@naver.com>
2021-07-18 12:03:54 +0900

    style : 웹 접근성을 위한 hidden 컴포넌트 추가
    - 대체 텍스트로 이용가능함.

---
+++ fitpet/src/common/accessibility/Hidden.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+// 웹 접근성을 위한 a11y-hidden style
+export const A11yHidden = styled.span`
+  overflow: hidden;
+  position: absolute;
+  clip: rect(0 0 0 0); /* IE 6,7 */
+  clip: rect(0, 0, 0, 0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+
+  &:focus {
+    overflow: visible;
+    position: static;
+    clip: auto;
+    width: auto;
+    height: auto;
+    margin: 0;
+  }
+`;
