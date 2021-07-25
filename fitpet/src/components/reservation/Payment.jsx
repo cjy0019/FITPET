ccc6249336a5b2e2ea287e4b8f3e24d0b50f9416
cjy0019 <cjy0029@naver.com>
2021-07-26 01:05:05 +0900

    feat : 결제 수단 라디오 버튼 커스터마이징 완료[#53]

--- fitpet/src/components/reservation/Payment.jsx
+++ fitpet/src/components/reservation/Payment.jsx
@@ -7,27 +7,39 @@ const Payment = () => {
       <h2>결제 수단 선택</h2>
       <SelectWrapper>
         <RadioWrapper>
-          <RadioButton id='kakaopay' value='kakaopay' />
-          <RadioLabel htmlFor='kakaopay'>카카오 페이</RadioLabel>
-
-          <RadioButton id='toss' value='toss' />
-          <RadioLabel htmlFor='toss'>토스</RadioLabel>
-
-          <RadioButton id='naverpay' value='naverpay' />
-          <RadioLabel htmlFor='naverpay'>네이버 페이</RadioLabel>
-
-          <RadioButton id='payco' value='payco' />
-          <RadioLabel htmlFor='payco'>PAYCO</RadioLabel>
+          <RadioLabel htmlFor='kakaopay'>
+            <RadioStyled type='radio' id='kakaopay' name='payments' />
+            <RadioCircle></RadioCircle>
+            카카오 페이
+          </RadioLabel>
+          <RadioLabel htmlFor='toss'>
+            <RadioStyled type='radio' id='toss' name='payments' />
+            <RadioCircle></RadioCircle>
+            토스
+          </RadioLabel>
+          <RadioLabel htmlFor='naverpay'>
+            <RadioStyled type='radio' id='naverpay' name='payments' />
+            <RadioCircle></RadioCircle>
+            네이버 페이
+          </RadioLabel>
+          <RadioLabel htmlFor='payco'>
+            <RadioStyled type='radio' id='payco' name='payments' />
+            <RadioCircle></RadioCircle>
+            PAYCO
+          </RadioLabel>
         </RadioWrapper>
 
         <RadioWrapperUnder>
-          <RadioButton id='credit' value='credit' />
-          <RadioLabel htmlFor='credit' m>
-            신용/체크 카드
+          <RadioLabel htmlFor='credit'>
+            <RadioStyled type='radio' id='credit' name='payments' />
+            <RadioCircle></RadioCircle>
+            신용/체크카드
           </RadioLabel>
-
-          <RadioButton id='phone' value='phone' />
-          <RadioLabel htmlFor='phone'>휴대폰 결제</RadioLabel>
+          <RadioLabel htmlFor='phone'>
+            <RadioStyled type='radio' id='phone' name='payments' />
+            <RadioCircle></RadioCircle>
+            휴대폰 결제
+          </RadioLabel>
         </RadioWrapperUnder>
       </SelectWrapper>
     </PaySection>
@@ -55,57 +67,50 @@ const RadioWrapper = styled.div`
 `;
 
 // 라디오 버튼 커스텀
-const RadioButton = styled.input.attrs({
-  type: 'radio',
-})`
-  margin: 0;
-  display: none;
-  cursor: pointer;
-
-  &:checked + label:before {
-    height: 1.2rem;
-    width: 1.2rem;
-    border-radius: 50%;
-    background-color: #01cc65;
-  }
-`;
-
-// 라벨링
 const RadioLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
   cursor: pointer;
   font-size: 1.6rem;
   font-weight: 500;
   line-height: 1.19;
-  display: inline;
+  color: #707070;
+  margin-right: 1rem;
+`;
 
-  &:before {
+const RadioStyled = styled.input`
+  display: none;
+`;
+
+const RadioCircle = styled.div`
+  width: 2.4rem;
+  height: 2.4rem;
+  padding: 0.4rem;
+  border: 2px solid #d8e4e2;
+  border-radius: 50%;
+  margin-right: 1rem;
+  box-sizing: border-box;
+
+  &::after {
     content: '';
-    display: inline-block;
-    width: 2.4rem;
-    height: 2.4rem;
-    border: solid 1px #979797;
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-color: #009879;
     border-radius: 50%;
-    margin-right: 1rem;
-    vertical-align: middle;
-  }
 
-  input[type='radio']:checked + label {
-    background-color: #01cc65;
-    color: white;
+    transform: scale(0);
   }
-
-  input[type='radio']:checked + label:before {
-    height: 1.6rem;
-    width: 1.6rem;
-    border: 10px solid white;
-    background-color: #01cc65;
+  input:checked + &::after {
+    transform: scale(1);
+    transition: transform 0.3s;
   }
 `;
 
 const RadioWrapperUnder = styled.div`
   display: flex;
   justify-content: space-between;
-  max-width: 37.4rem;
+  max-width: 38.1rem;
   margin-top: 3.1rem;
 `;
 
