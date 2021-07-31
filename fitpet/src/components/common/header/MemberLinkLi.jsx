f9073d2bee08f791f0f97649e2298b298cf710a4
cjy0019 <cjy0029@naver.com>
2021-08-01 03:29:02 +0900

    style : img 폴더 디렉터리 구조 변경.

--- fitpet/src/components/common/header/MemberLinkLi.jsx
+++ fitpet/src/components/common/header/MemberLinkLi.jsx
@@ -7,21 +7,21 @@ const MemberLinkLi = ({ showSignUp, showLogin }) => {
     <>
       <StyeldLi>
         <NavLink to='/'>
-          <StyledImg src={'/img/searchImg.svg'} alt='검색' />
+          <StyledImg src={'/img/header/searchImg.svg'} alt='검색' />
           <p>검색</p>
         </NavLink>
       </StyeldLi>
 
       <StyeldLi>
-        <StyledButton onClick={showSignUp} img='/img/signInImg.svg'>
-          <StyledImg src={'/img/signInImg.svg'} alt='회원가입' />
+        <StyledButton onClick={showSignUp} img='/img/header/signInImg.svg'>
+          <StyledImg src={'/img/header/signInImg.svg'} alt='회원가입' />
           <p>회원가입</p>
         </StyledButton>
       </StyeldLi>
 
       <StyeldLi>
         <StyledButton onClick={showLogin}>
-          <StyledImg src={'/img/loginImg.svg'} alt='로그인' />
+          <StyledImg src={'/img/header/loginImg.svg'} alt='로그인' />
           <p>로그인</p>
         </StyledButton>
       </StyeldLi>
@@ -35,7 +35,7 @@ const MemberLinkLi = ({ showSignUp, showLogin }) => {
 
       <StyeldLi style={{ marginRight: 0 }}>
         <NavLink to='/'>
-          <StyledImg src={'/img/cartImg.svg'} alt='장바구니' />
+          <StyledImg src={'/img/header/cartImg.svg'} alt='장바구니' />
           <p>장바구니</p>
         </NavLink>
       </StyeldLi>
--- fitpet/src/components/mainpage/slider/sliderData.js
+++ fitpet/src/components/mainpage/slider/sliderData.js
@@ -1,42 +1,42 @@
 // 어디로 갈까요 데이터
 export const locationSlider = [
   {
-    img: '/img/where1.png',
+    img: '/img/main/location_slider/where1.png',
     title: '제주도',
     tag1: '#섬',
     tag2: '#아름다운',
     tag3: '#바닷가',
   },
   {
-    img: '/img/where2.png',
+    img: '/img/main/location_slider/where2.png',
     title: '강릉',
     tag1: '#동해',
     tag2: '#양떼목장',
     tag3: '#바닷가',
   },
   {
-    img: '/img/where3.png',
+    img: '/img/main/location_slider/where3.png',
     title: '대구',
     tag1: '#여름',
     tag2: '#카페',
     tag3: '',
   },
   {
-    img: '/img/where4.png',
+    img: '/img/main/location_slider/where4.png',
     title: '부산',
     tag1: '#모래사장',
     tag2: '#수영',
     tag3: '',
   },
   {
-    img: '/img/where2.png',
+    img: '/img/main/location_slider/where2.png',
     title: '강릉',
     tag1: '#동해',
     tag2: '#양떼목장',
     tag3: '#바닷가',
   },
   {
-    img: '/img/where1.png',
+    img: '/img/main/location_slider/where1.png',
     title: '제주도',
     tag1: '#섬',
     tag2: '#아름다운',
@@ -47,32 +47,32 @@ export const locationSlider = [
 // 오직 핏펫에서만 슬라이드 데이터
 export const onlySlider = [
   {
-    img: '/img/onlySlide.png',
+    img: '/img/main/only_slider/onlySlide.png',
     text1: '올 여름은',
     text2: '애견수영장에서 시원하게',
   },
   {
-    img: '/img/onlySlide2.png',
+    img: '/img/main/only_slider/onlySlide2.png',
     text1: '제주도 반려동물',
     text2: '동반펜션 TOP5',
   },
   {
-    img: '/img/onlySlide3.png',
+    img: '/img/main/only_slider/onlySlide3.png',
     text1: '시원한 할인 이벤트',
     text2: '캠핑 떠나자!',
   },
   {
-    img: '/img/onlySlide.png',
+    img: '/img/main/only_slider/onlySlide.png',
     text1: '올 여름은',
     text2: '애견수영장에서 시원하게',
   },
   {
-    img: '/img/onlySlide2.png',
+    img: '/img/main/only_slider/onlySlide2.png',
     text1: '제주도 반려동물',
     text2: '동반펜션 TOP5',
   },
   {
-    img: '/img/onlySlide3.png',
+    img: '/img/main/only_slider/onlySlide3.png',
     text1: '시원한 할인 이벤트',
     text2: '캠핑 떠나자!',
   },
