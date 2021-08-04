aeecda23e6ec379ef5521cebcf0b25098b64bc87
cjy0019 <cjy0029@naver.com>
2021-08-05 02:49:42 +0900

    config : redux, connectedRouter 설정 완료[#90]
    resolved : #90
    
    - redux > modules > reducer.js / saga.js 로 분리.
    - connectedRouter 설정으로 history 객체 리덕스로 관리가능.

--- fitpet/src/App.js
+++ fitpet/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import NotFoundPage from './pages/NotFoundPage';
 import MainPage from './pages/MainPage';
@@ -11,39 +11,43 @@ import ReservationPage from './pages/ReservationPage';
 import HotelAboutPage from './pages/HotelAboutPage';
 import HotelListPage from './pages/HotelListPage';
 import RoomDetailModalPage from './pages/RoomDetailModalPage';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
+import { Provider } from 'react-redux';
 import RentCarDetailPage from './pages/RentCarDetailPage';
 import RentCarPage from './pages/RentCarPage';
 import HotelMainPage from './pages/HotelMainPage';
 import RentalcarMainPage from './pages/RentalcarMainPage';
 import DriverInfoPage from './pages/DriverInfoPage';
+import { createBrowserHistory } from 'history';
+import create from './redux/create';
+import { ConnectedRouter } from 'connected-react-router';
 
-// store = { store };
+const history = createBrowserHistory();
+
+const store = create(history);
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorPage}>
-      {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <Switch>
-          <Route component={MainPage} exact path='/' />
-          <Route component={SignUpPage} path='/signup' />
-          <Route component={CommonPage} path='/common' />
-          <Route component={ReservationPage} path='/reservation' />
-          <Route component={HotelMainPage} path='/hotel' />
-          <Route component={HotelAboutPage} path='/hotelAbout' />
-          <Route component={RoomDetailModalPage} path='/roomDetail' />
-          <Route component={HotelListPage} path='/hotelList' />
-          <Route component={RentalcarMainPage} path='/rentalcar' />
-          <Route component={RentCarPage} path='/rentcar' />
-          <Route component={RentCarDetailPage} path='/rentcardetail' />
-          <Route component={DriverInfoPage} path='/driver' />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </BrowserRouter>
-      <GlobalStyles />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route component={MainPage} exact path='/' />
+            <Route component={SignUpPage} path='/signup' />
+            <Route component={CommonPage} path='/common' />
+            <Route component={ReservationPage} path='/reservation' />
+            <Route component={HotelMainPage} path='/hotel' />
+            <Route component={HotelAboutPage} path='/hotelAbout' />
+            <Route component={RoomDetailModalPage} path='/roomDetail' />
+            <Route component={HotelListPage} path='/hotelList' />
+            <Route component={RentalcarMainPage} path='/rentalcar' />
+            <Route component={RentCarPage} path='/rentcar' />
+            <Route component={RentCarDetailPage} path='/rentcardetail' />
+            <Route component={DriverInfoPage} path='/driver' />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </ConnectedRouter>
+        <GlobalStyles />
+      </Provider>
     </ErrorBoundary>
   );
 }
