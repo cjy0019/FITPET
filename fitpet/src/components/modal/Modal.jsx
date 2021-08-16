4af304795aa69cb629d3bce605b6d884077421f1
cjy0019 <cjy0029@naver.com>
2021-08-17 00:29:17 +0900

    fix : 모달창에서 다른 모달창으로 이동할시 깜빡거리는 현상 해결.
    - modal창마다 존재했던 Overlay 컴포넌트를 공통으로 관리.

--- fitpet/src/components/modal/Modal.jsx
+++ fitpet/src/components/modal/Modal.jsx
@@ -1,21 +1,23 @@
 // Modal.js
-import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import styled from 'styled-components';
 // We get hold of the div with the id modal that we have created in index.html
 
 const Modal = (props) => {
+  // modal id값의 DOM을 취득한다.
   const modalRoot = document.getElementById('modal');
-  const element = document.createElement('div');
 
-  useEffect(() => {
-    modalRoot.appendChild(element);
-
-    return () => {
-      modalRoot.appendChild(element);
-    };
-  });
-
-  return createPortal(props.children, element);
+  return createPortal(<Overlay>{props.children}</Overlay>, modalRoot);
 };
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 999999;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
 
 export default Modal;
