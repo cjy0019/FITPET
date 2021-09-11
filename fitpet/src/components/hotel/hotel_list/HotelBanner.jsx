ce8d7f0dc9efaa566a51f27bf5c9c9bb0d0b97d4
GoDoyeon <97godo@naver.com>
2021-09-11 16:52:14 +0900

    fix : 검색 창 하얗게 (호텔 어바웃과 동일하게 수정)

--- fitpet/src/components/hotel/hotel_list/HotelBanner.jsx
+++ fitpet/src/components/hotel/hotel_list/HotelBanner.jsx
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import useWindowScroll from '../../../hooks/useWindowScroll';
 import MainNav from '../../mainpage/banner/MainNav';
+import HotelDate from '../hotel_common/HotelDate';
 import HotelSearch from '../hotel_common/HotelSearch';
 
 const HotelBanner = () => {
   const yOffset = useWindowScroll();
   const [isScroll, setIsScroll] = useState(true);
 
+  // 210911 by.dy
+  // 스크롤 시 헤더에 붙어있는 날짜 헤더
+  const [isScrollDate, setIsScrollDate] = useState(true);
+
   useEffect(() => {
-    if (yOffset > 10) setIsScroll(false);
-    else setIsScroll(true);
+    if (yOffset > 20) {
+      setIsScroll(false);
+      setIsScrollDate(true);
+    } else {
+      setIsScroll(true);
+      setIsScrollDate(false);
+    }
   }, [yOffset]);
 
   return (
@@ -18,11 +28,17 @@ const HotelBanner = () => {
       {isScroll ? (
         <Banner>
           <MainNav />
-          <HotelSearch isScroll={isScroll} />
+          <div className='bannerMain'>
+            <HotelSearch isScroll={isScroll} />
+          </div>
         </Banner>
       ) : (
         <Banner scroll>
-          <HotelSearch isScroll={isScroll} />
+          {/* 210911 by.dy 
+          호버시 박스 쉐도우 스타일 적용 추가 */}
+          <div className='bannerDate'>
+            <HotelDate isScroll={isScrollDate} />
+          </div>
         </Banner>
       )}
     </>
@@ -37,16 +53,21 @@ const Banner = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-bottom: 2.3rem;
   background-size: cover;
   background-repeat: no-repeat;
   background-image: url('/img/hotel/hotel_list/hotelsearchbanner.png');
-
-  ${(props) =>
-    props.scroll &&
-    css`
-      padding: 1.9rem 0 1.2rem;
-    `}
+  .bannerMain {
+    padding-bottom: 2.3rem;
+  }
+  .bannerDate {
+    top: 8rem;
+    background-color: #fff;
+    width: 100%;
+    height: 8rem;
+    padding-right: 60rem;
+    display: -webkit-box; // 오른쪽으로 붙음
+    box-shadow: 0 3px 10px -5px rgba(0, 0, 0, 0.16);
+  }
 `;
 
 export default HotelBanner;
